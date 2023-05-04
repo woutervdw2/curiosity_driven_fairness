2037ca9de9de0135a5877871fbb6f247c1d73059
woutervdw2 <woutervdw2@computer.com>
2023-05-04 15:15:33 +0200

    Eval plots

--- environments/lending_params.py
+++ environments/lending_params.py
@@ -200,13 +200,15 @@ DELAYED_IMPACT_CLUSTER_PROBS = (
     (0.1, 0.1, 0.2, 0.3, 0.3, 0.0, 0.0),
 )
 # Likelihoods of loan repayment given credit score.
+#Original
 DELAYED_IMPACT_SUCCESS_PROBS = (0.1, 0.2, 0.45, 0.6, 0.65, 0.7, 0.7)
-
+#Boosted lower scores
+BOOSTED_DELAYED_IMPACT_SUCCESS_PROBS = (0.2, 0.3, 0.55, 0.6, 0.65, 0.7, 0.7)
 
 def two_group_credit_clusters(
     group_likelihoods=(0.5, 0.5),
     cluster_probabilities=DELAYED_IMPACT_CLUSTER_PROBS,
-    success_probabilities=DELAYED_IMPACT_SUCCESS_PROBS):
+    success_probabilities=BOOSTED_DELAYED_IMPACT_SUCCESS_PROBS):
   """Returns a mixture of two credit cluster distributions.
 
   Args:
